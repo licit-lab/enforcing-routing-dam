ae5d74f42066446b66f1022de4a6628df3635210
Andres Ladino <aladinoster@gmail.com>
2020-04-11 09:36:33 +0200

    ✨ Control algorithm, proportional version

--- src/algorithms/control.py
+++ src/algorithms/control.py
@@ -5,6 +5,8 @@
 import numpy as np
 import networkx as nx
 from scipy.sparse import csr_matrix
+from itertools import repeat
+from .pid import PIDantiwindup
 
 # ======================================================================================================================
 # Constants
@@ -23,13 +25,26 @@ DEFAULT_MAP = {
     (2, 2): "Cpt_9",
 }
 
+
+DEFAULT_SPD = {
+    "Cpt_1": 6.3,
+    "Cpt_2": 6.3,
+    "Cpt_3": 6.1,
+    "Cpt_4": 6.4,
+    "Cpt_5": 5.5,
+    "Cpt_6": 5.4,
+    "Cpt_8": 5.6,
+    "Cpt_7": 5.2,
+    "Cpt_9": 6.2,
+}
+
 # ======================================================================================================================
 # Functions
 # ======================================================================================================================
 
 
 # Cooperative graph
-def define_grid_graph(N: int = 3, M: int = 3, mapping: dict = DEFAULT_MAP) -> nx.Graph:
+def define_grid_graph(N: int = 3, M: int = 3, mapping: dict = DEFAULT_MAP, refSpeed: dict = DEFAULT_SPD) -> nx.Graph:
     """ Creates a grid graph for the control algorithm 
     
     :param N: row number, defaults to 3
@@ -50,6 +65,9 @@ def define_grid_graph(N: int = 3, M: int = 3, mapping: dict = DEFAULT_MAP) -> nx
 
     # Explicit
     G = nx.relabel_nodes(G, mapping)
+
+    # Set reference speed on node
+    nx.set_node_attributes(G, refSpeed, "freeFlowSpeed")
 
     # Plot graph
     # nx.draw(G, node_color="#A0CBE2", with_labels=True)
@@ -72,15 +90,15 @@ def get_graph_data(G: nx.Graph) -> tuple:
     epsilon = 1 / (10 * delta)
 
     N = len(G.nodes)
-    P = np.identity(N * N) - epsilon * L
+    P = np.identity(N) - epsilon * L
 
     return P, L, epsilon
 
 
-def compute_vanishing_control(speeds: dict, G: nx.Graph) -> dict:
+def compute_vanishing_control(simulationstep: int, triggertime: int, speeds: list, G: nx.Graph) -> dict:
     """ Computes the control law based on current speeds and some graph information
 
-    :param speeds: dictionary of speed per zone
+    :param speeds: list of cumulated speeds
     :type speeds: dict
     :param G: undirected graph for cooperativity 
     :type G: nx.Graph
@@ -88,7 +106,37 @@ def compute_vanishing_control(speeds: dict, G: nx.Graph) -> dict:
     :rtype: dict
     """
 
+    # Case speed is empty
+    if not speeds:
+        return dict(zip(list(G.nodes), repeat(0)))
+
     # Network layout creation
-    _, L, epsilon = get_graph_data(G)  # Works because the graph is small
-    control = epsilon * L.toarray() @ np.array(list(speeds.values()))  # Consensus protocol
-    return dict(zip(speeds.keys(), control))
+    P, L, epsilon = get_graph_data(G)  # Works because the graph is small
+
+    # Compute normalized states
+    normState = np.array([speeds[-1][s] / G.nodes[s]["freeFlowSpeed"] for s in G.nodes])
+
+    # Compute local control
+    localControl = 0.5 * np.maximum(1 - normState, 0)
+
+    # Compute neighbor information
+    neighControl = 0.5 * epsilon * L @ normState
+
+    totalControl = localControl + np.maximum(neighControl, 0)
+
+    return dict(zip(speeds[-1].keys(), totalControl))
+
+
+def compute_pid_vanishing_control(simulationstep: int, triggertime: int, speeds: list, G: nx.Graph) -> dict:
+    """    
+    :param speeds: [description]
+    :type speeds: dict
+    :param G: [description]
+    :type G: nx.Graph
+    :return: [description]
+    :rtype: dicts
+    """
+
+    # PID
+
+    u = PIDantiwindup(k_p=0.1, k_d=0.1, k_i=0.1)
