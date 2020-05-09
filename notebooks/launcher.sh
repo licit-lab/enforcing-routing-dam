d7552222eabd5ce0751d597d2d0c70a70206a05f
Andres Ladino <aladinoster@gmail.com>
2020-05-09 21:47:51 +0200

    🎨 Uncomment instructions

--- notebooks/launcher.sh
+++ notebooks/launcher.sh
@@ -5,10 +5,10 @@ PATH_SYMUVIA="/home/ladino/dev-symuvia/build/lib/libSymuVia.so"
 for case in CO_P CO_PI P PI OPENL
 do
    if [ "$case" == "OL" ]; then 
-        echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
-   -p PATH_SYMUVIA ${PATH_SYMUVIA} -p CONTROL_TYPE ${case} &"
+        papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+   -p PATH_SYMUVIA ${PATH_SYMUVIA} -p CONTROL_TYPE ${case} &
    else
-       echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
-   -p CASE ${case} -p PATH_SYMUVIA ${PATH_SYMUVIA} &"
+       papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+   -p CASE ${case} -p PATH_SYMUVIA ${PATH_SYMUVIA} &
    fi
 done
