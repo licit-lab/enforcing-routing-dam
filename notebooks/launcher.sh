67fb4da2bc67f80a2f2a4b881f002d64f06ffc7e
Andres Ladino <aladinoster@gmail.com>
2020-05-11 18:00:17 +0200

    🎨  Organising launcher

--- notebooks/launcher.sh
+++ notebooks/launcher.sh
@@ -14,8 +14,8 @@ do
     -p REF_SPEED "ref_speeds_9zones.csv" &"   
     papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    -p FILE "symuvia_network_9zones.xml" \
     -p CONTROL_TYPE ${case} \
+    -p FILE "symuvia_network_9zones.xml" \
     -p SCENARIO "mesh9x9/" \
     -p ZONES "9zones/" \
     -p REF_SPEED "ref_speeds_9zones.csv" &
@@ -28,7 +28,11 @@ do
     -p ZONES "9zones/" \
     -p REF_SPEED "ref_speeds_9zones.csv" &"
     papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
-    -p CASE ${case} -p PATH_SYMUVIA ${PATH_SYMUVIA} -p FILE #"symuvia_network_9zones.xml" \
-    -p SCENARIO "mesh9x9/" -p ZONES "9zones/" -p REF_SPEED "ref_speeds_9zones.csv" &
+    -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    -p CASE ${case} \
+    -p FILE "symuvia_network_9zones.xml" \
+    -p SCENARIO "mesh9x9/" \
+    -p ZONES "9zones/" \
+    -p REF_SPEED "ref_speeds_9zones.csv" &"
    fi
 done
