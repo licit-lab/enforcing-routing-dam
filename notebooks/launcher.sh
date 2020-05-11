a2e0941a453592ece5c10dd1640ec1665521b7e1
Andres Ladino <aladinoster@gmail.com>
2020-05-11 17:07:54 +0200

    🧪  Experiment 9x9

--- notebooks/launcher.sh
+++ notebooks/launcher.sh
@@ -5,10 +5,12 @@ PATH_SYMUVIA="/home/ladino/dev-symuvia/build/lib/libSymuVia.so"
 for case in CO_P CO_PI P PI OPENL
 do
    if [ "$case" == "OL" ]; then 
-        papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
-   -p PATH_SYMUVIA ${PATH_SYMUVIA} -p CONTROL_TYPE ${case} &
+    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+   -p PATH_SYMUVIA ${PATH_SYMUVIA} -p FILE "symuvia_network_9zones.xml" -p CONTROL_TYPE ${case} \
+   -p SCENARIO "mesh9x9/" -p ZONES "9zones/" -p REF_SPEED "ref_speeds_9zones.csv" &
    else
-       papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
-   -p CASE ${case} -p PATH_SYMUVIA ${PATH_SYMUVIA} -p CO_TI 600&
+    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+   -p CASE ${case} -p PATH_SYMUVIA ${PATH_SYMUVIA} -p FILE "symuvia_network_9zones.xml" \
+   -p SCENARIO "mesh9x9/" -p ZONES "9zones/" -p REF_SPEED "ref_speeds_9zones.csv" &
    fi
 done
