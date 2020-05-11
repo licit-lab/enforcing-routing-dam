c007764b927a81a92510d5175f3cb4ce9481adf7
Andres Ladino <aladinoster@gmail.com>
2020-05-11 17:44:32 +0200

    🚑  Typo speed file

--- notebooks/launcher.sh
+++ notebooks/launcher.sh
@@ -29,6 +29,6 @@ do
     -p REF_SPEED "ref_speeds_9zones.csv" &"
     papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
     -p CASE ${case} -p PATH_SYMUVIA ${PATH_SYMUVIA} -p FILE #"symuvia_network_9zones.xml" \
-    -p SCENARIO "mesh9x9/" -p ZONES "9zones/" -p REF_SPEED "ref_speeds_9zones.#csv" &
+    -p SCENARIO "mesh9x9/" -p ZONES "9zones/" -p REF_SPEED "ref_speeds_9zones.csv" &
    fi
 done
