5de0e15325878db9d19727b24d634c5f376d71ac
Andres Ladino <aladinoster@gmail.com>
2020-05-11 17:40:19 +0200

    🐛  Launcher organisation

--- notebooks/launcher.sh
+++ notebooks/launcher.sh
@@ -4,13 +4,31 @@ PATH_SYMUVIA="/home/ladino/dev-symuvia/build/lib/libSymuVia.so"
 
 for case in CO_P CO_PI P PI OPENL
 do
-   if [ "$case" == "OL" ]; then 
-    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
-   -p PATH_SYMUVIA ${PATH_SYMUVIA} -p FILE "symuvia_network_9zones.xml" -p CONTROL_TYPE ${case} \
-   -p SCENARIO "mesh9x9/" -p ZONES "9zones/" -p REF_SPEED "ref_speeds_9zones.csv" &
+   if [ "$case" == "OPENL" ]; then 
+    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+    -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    -p FILE "symuvia_network_9zones.xml" \
+    -p CONTROL_TYPE ${case} \
+    -p SCENARIO "mesh9x9/" \
+    -p ZONES "9zones/" \
+    -p REF_SPEED "ref_speeds_9zones.csv" &"   
+    # papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+    #-p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    #-p FILE "symuvia_network_9zones.xml" \
+    #-p #CONTROL_TYPE ${case} \
+    #-p SCENARIO "mesh9x9/" \
+    #-p ZONES "9zones/" \
+    #-p REF_SPEED "ref_speeds_9zones.#csv" &
    else
-    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
-   -p CASE ${case} -p PATH_SYMUVIA ${PATH_SYMUVIA} -p FILE "symuvia_network_9zones.xml" \
-   -p SCENARIO "mesh9x9/" -p ZONES "9zones/" -p REF_SPEED "ref_speeds_9zones.csv" &
+    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+    -p CASE ${case} \
+    -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    -p FILE "symuvia_network_9zones.xml" \
+    -p SCENARIO "mesh9x9/" \
+    -p ZONES "9zones/" \
+    -p REF_SPEED "ref_speeds_9zones.csv" &"
+    # papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+    #-p CASE ${case} -p PATH_SYMUVIA ${PATH_SYMUVIA} -p FILE #"symuvia_network_9zones.xml" \
+    #-p SCENARIO "mesh9x9/" -p ZONES "9zones/" -p REF_SPEED "ref_speeds_9zones.#csv" &
    fi
 done
