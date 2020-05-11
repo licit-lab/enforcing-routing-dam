365fdf210b8c0babdb0b045ffb656872296c46db
Andres Ladino <aladinoster@gmail.com>
2020-05-11 17:41:23 +0200

    🎨 Launcher active

--- notebooks/launcher.sh
+++ notebooks/launcher.sh
@@ -12,13 +12,13 @@ do
     -p SCENARIO "mesh9x9/" \
     -p ZONES "9zones/" \
     -p REF_SPEED "ref_speeds_9zones.csv" &"   
-    # papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
-    #-p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    #-p FILE "symuvia_network_9zones.xml" \
-    #-p #CONTROL_TYPE ${case} \
-    #-p SCENARIO "mesh9x9/" \
-    #-p ZONES "9zones/" \
-    #-p REF_SPEED "ref_speeds_9zones.#csv" &
+    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+    -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    -p FILE "symuvia_network_9zones.xml" \
+    -p #CONTROL_TYPE ${case} \
+    -p SCENARIO "mesh9x9/" \
+    -p ZONES "9zones/" \
+    -p REF_SPEED "ref_speeds_9zones.#csv" &
    else
     echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
     -p CASE ${case} \
@@ -27,8 +27,8 @@ do
     -p SCENARIO "mesh9x9/" \
     -p ZONES "9zones/" \
     -p REF_SPEED "ref_speeds_9zones.csv" &"
-    # papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
-    #-p CASE ${case} -p PATH_SYMUVIA ${PATH_SYMUVIA} -p FILE #"symuvia_network_9zones.xml" \
-    #-p SCENARIO "mesh9x9/" -p ZONES "9zones/" -p REF_SPEED "ref_speeds_9zones.#csv" &
+    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+    -p CASE ${case} -p PATH_SYMUVIA ${PATH_SYMUVIA} -p FILE #"symuvia_network_9zones.xml" \
+    -p SCENARIO "mesh9x9/" -p ZONES "9zones/" -p REF_SPEED "ref_speeds_9zones.#csv" &
    fi
 done
