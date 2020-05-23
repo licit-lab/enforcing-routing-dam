d7a32e0f65c31b76ce686f5bcf92547bfeb54650
Andres Ladino <aladinoster@gmail.com>
2020-05-23 02:19:42 +0200

    🔧 Simulation parametrization

--- notebooks/launcher.sh
+++ notebooks/launcher.sh
@@ -7,14 +7,40 @@ do
    if [ "$case" == "OPENL" ]; then 
     echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    -p CTR_ALG ${case} \
+    -p CONTROL_MODE ${case} &"
+      papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+    -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    -p CTR_ALG ${case} \
+    -p CONTROL_MODE ${case} &
+   else
+    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+    -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    -p CTR_ALG ${case} &"
+      papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+    -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    -p CTR_ALG ${case} &
+   fi
+done  
+
+
+for case in CO_P CO_PI P PI OPENL
+do
+   if [ "$case" == "OPENL" ]; then 
+   echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+    -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    -p CTR_ALG ${case} \
+    -p CONTROL_MODE ${case} \
     -p FILE "symuvia_network_9zones.xml" \
-    -p CONTROL_TYPE ${case} \
+    -p NRow 3 \
+    -p NCol 3 \
     -p SCENARIO "mesh9x9/" \
     -p ZONES "9zones/" \
-    -p REF_SPEED "ref_speeds_9zones.csv" &"   
-    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+    -p REF_SPEED "ref_speeds_9zones.csv" &"
+      papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    -p CONTROL_TYPE ${case} \
+    -p CTR_ALG ${case} \
+    -p CONTROL_MODE ${case} \
     -p FILE "symuvia_network_9zones.xml" \
     -p NRow 3 \
     -p NCol 3 \
@@ -23,15 +49,17 @@ do
     -p REF_SPEED "ref_speeds_9zones.csv" &
    else
     echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
-    -p CASE ${case} \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    -p CTR_ALG ${case} \
     -p FILE "symuvia_network_9zones.xml" \
+    -p NRow 3 \
+    -p NCol 3 \
     -p SCENARIO "mesh9x9/" \
     -p ZONES "9zones/" \
     -p REF_SPEED "ref_speeds_9zones.csv" &"
-    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+      papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    -p CASE ${case} \
+    -p CTR_ALG ${case} \
     -p FILE "symuvia_network_9zones.xml" \
     -p NRow 3 \
     -p NCol 3 \
