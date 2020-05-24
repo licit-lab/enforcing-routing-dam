45071ec1b94ff5bc49e7ba5877ab3d4d0b6667bd
Andres Ladino <aladinoster@gmail.com>
2020-05-24 06:54:45 +0200

    ✏️ Renaming nb

--- notebooks/launcher.sh
+++ notebooks/launcher.sh
@@ -5,19 +5,19 @@ PATH_SYMUVIA="/home/ladino/dev-symuvia/build/lib/libSymuVia.so"
 # for case in CO_P CO_PI P PI OPENL
 # do
 #    if [ "$case" == "OPENL" ]; then 
-#     echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+#     echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_55.ipynb \
 #     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
 #     -p CTR_ALG ${case} \
 #     -p CONTROL_MODE ${case} &"
-#       papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+#       papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_55.ipynb \
 #     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
 #     -p CTR_ALG ${case} \
 #     -p CONTROL_MODE ${case} &
 #    else
-#     echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+#     echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_55.ipynb \
 #     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
 #     -p CTR_ALG ${case} &"
-#       papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+#       papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_55.ipynb \
 #     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
 #     -p CTR_ALG ${case} &
 #    fi
@@ -27,7 +27,7 @@ PATH_SYMUVIA="/home/ladino/dev-symuvia/build/lib/libSymuVia.so"
 # for case in CO_P CO_PI P PI OPENL
 # do
 #    if [ "$case" == "OPENL" ]; then 
-#    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+#    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_33.ipynb \
 #     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
 #     -p CTR_ALG ${case} \
 #     -p CONTROL_MODE ${case} \
@@ -37,7 +37,7 @@ PATH_SYMUVIA="/home/ladino/dev-symuvia/build/lib/libSymuVia.so"
 #     -p SCENARIO "mesh9x9/" \
 #     -p ZONES "9zones/" \
 #     -p REF_SPEED "ref_speeds_9zones.csv" &"
-#       papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+#       papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_33.ipynb \
 #     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
 #     -p CTR_ALG ${case} \
 #     -p CONTROL_MODE ${case} \
@@ -48,7 +48,7 @@ PATH_SYMUVIA="/home/ladino/dev-symuvia/build/lib/libSymuVia.so"
 #     -p ZONES "9zones/" \
 #     -p REF_SPEED "ref_speeds_9zones.csv" &
 #    else
-#     echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+#     echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_33.ipynb \
 #     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
 #     -p CTR_ALG ${case} \
 #     -p FILE "symuvia_network_9zones.xml" \
@@ -57,7 +57,7 @@ PATH_SYMUVIA="/home/ladino/dev-symuvia/build/lib/libSymuVia.so"
 #     -p SCENARIO "mesh9x9/" \
 #     -p ZONES "9zones/" \
 #     -p REF_SPEED "ref_speeds_9zones.csv" &"
-#       papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}.ipynb \
+#       papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_33.ipynb \
 #     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
 #     -p CTR_ALG ${case} \
 #     -p FILE "symuvia_network_9zones.xml" \
