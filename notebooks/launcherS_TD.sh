f7af4fbc9b26e91e3037b3e836bd994384581ba8
Andres Ladino <aladinoster@gmail.com>
2020-06-07 13:57:54 +0200

    🧪  PD: Scenario A + B

--- notebooks/launcherS_TD.sh
+++ notebooks/launcherS_TD.sh
@@ -31,3 +31,30 @@ do
 done
 
 echo "Total Simulations: ${IT}"
+
+# Scenario B
+
+case="PD"
+
+for k in 200 250 300 350 400 500
+do
+    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_TD_${k}_dB.ipynb \
+    -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    -p EXPERIMENT CTR_SCNATD_${k} \
+    -p CTR_ALG ${case} \
+    -p KP 0.15 \
+    -p TD ${k} \
+    -p FILE "manhattan_grid_5X5_B.xml" \
+    -p DEMAND_FILE "demand_scenario_B.csv" &"
+    IT=$((IT+1))
+    papermill 01_Zone_Control.ipynb 01_Zone_Control_TD_${k}_dB.ipynb \
+    -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    -p EXPERIMENT CTR_SCNATD_${k} \
+    -p CTR_ALG ${case} \
+    -p KP 0.15 \
+    -p TD ${k} \
+    -p FILE "manhattan_grid_5X5_B.xml" \
+    -p DEMAND_FILE "demand_scenario_B.csv" &       
+done
+
+echo "Total Simulations: ${IT}"
