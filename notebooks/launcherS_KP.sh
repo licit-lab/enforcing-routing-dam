e7c50fe5b0e2c10085facca4a21f3376050c6405
Andres Ladino <aladinoster@gmail.com>
2020-06-08 12:36:02 +0200

    🧪 Test SensKP: A+B + (C)

--- notebooks/launcherS_KP.sh
+++ notebooks/launcherS_KP.sh
@@ -9,44 +9,66 @@ IT=0
 
 case="P"
 
-for k in  0.01 0.05 0.1 0.15 0.2
+for k in 0.01 0.05 0.1 0.15 0.17 0.19 0.2 0.21 0.23 0.25 0.3 0.5
 do
-    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_KP_${k}_dA.ipynb \
+    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_KP_${k}_SCNAD800.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    -p EXPERIMENT CTR_SCNAKP_${k} \
+    -p EXPERIMENT SKP_SCNAD800_${k} \
     -p CTR_ALG ${case} \
     -p KP ${k} \
-    -p FILE "manhattan_grid_5X5_A.xml" \
+    -p FILE "manhattan_grid_5X5_scenario_A.xml" \
     -p DEMAND_FILE "demand_scenario_A.csv" &"
     IT=$((IT+1))
-    papermill 01_Zone_Control.ipynb 01_Zone_Control_KP_${k}_dA.ipynb \
+    papermill 01_Zone_Control.ipynb 01_Zone_Control_KP_${k}_SCNAD800.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    -p EXPERIMENT CTR_SCNAKP_${k} \
+    -p EXPERIMENT SKP_SCNAD800_${k} \
     -p CTR_ALG ${case} \
     -p KP ${k} \
-    -p FILE "manhattan_grid_5X5_A.xml" \
-    -p DEMAND_FILE "demand_scenario_A.csv" &    
+    -p FILE "manhattan_grid_5X5_scenario_A.xml" \
+    -p DEMAND_FILE "demand_scenario_A.csv" &
 done
 
 # Scenario B
 
-for k in  0.01 0.05 0.1 0.15 0.2
+for k in 0.01 0.05 0.1 0.15 0.17 0.19 0.2 0.21 0.23 0.25 0.3 0.5
 do
-    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_KP_${k}_dB.ipynb \
+    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_KP_${k}_SCNBD800.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    -p EXPERIMENT CTR_SCNBKP_${k} \
+    -p EXPERIMENT SKP_SCNBD800_${k} \
     -p CTR_ALG ${case} \
     -p KP ${k} \
     -p FILE "manhattan_grid_5X5_B.xml" \
     -p DEMAND_FILE "demand_scenario_B.csv" &"
     IT=$((IT+1))
-    papermill 01_Zone_Control.ipynb 01_Zone_Control_KP_${k}_dB.ipynb \
+    papermill 01_Zone_Control.ipynb 01_Zone_Control_KP_${k}_SCNBD800.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    -p EXPERIMENT CTR_SCNBKP_${k} \
+    -p EXPERIMENT SKP_SCNBD800_${k} \
     -p CTR_ALG ${case} \
     -p KP ${k} \
-    -p FILE "manhattan_grid_5X5_B.xml" \
-    -p DEMAND_FILE "demand_scenario_B.csv" &    
+    -p FILE "manhattan_grid_5X5_scenario_B.xml" \
+    -p DEMAND_FILE "demand_scenario_B.csv" &     
 done
+
+# Scenario C
+
+# for k in  0.01 0.05 0.1 0.15 0.17 0.19 0.2 0.21 0.23 0.25 0.3 0.5
+# do
+#     echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_KP_${k}_SCNCD800.ipynb \
+#     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+#     -p EXPERIMENT SKP_SCNCD800_${k} \
+#     -p CTR_ALG ${case} \
+#     -p KP ${k} \
+#     -p FILE "manhattan_grid_5X5_C.xml" \
+#     -p DEMAND_FILE "demand_scenario_C.csv" &"
+#     IT=$((IT+1))
+#     papermill 01_Zone_Control.ipynb 01_Zone_Control_KP_${k}_SCNCD800.ipynb \
+#     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+#     -p EXPERIMENT SKP_SCNCD800_${k} \
+#     -p CTR_ALG ${case} \
+#     -p KP ${k} \
+#     -p FILE "manhattan_grid_5X5_C.xml" \
+#     -p DEMAND_FILE "demand_scenario_C.csv" &   
+# done
+
 
 echo "Total Simulations: ${IT}"
