ab578a7958d429f3571165dede8491645f46924c
Andres Ladino <aladinoster@gmail.com>
2020-06-10 14:56:06 +0200

    🧪 Test COST: COST 3-6 A+B

--- notebooks/launcherCOST.sh
+++ notebooks/launcherCOST.sh
@@ -3,27 +3,68 @@
 PATH_SYMUVIA="/home/ladino/dev-symuvia/build/lib/libSymuVia.so"
 IT=0
 
-# Test 3 cooperative strategies 
+# Cooperative strategies 
 
 # Scenario A
 
-for case in COST1 COST2 COST3
+for case in COST2 COST3 COST5 COST6
 do
-    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_5x5_cost_dA.ipynb \
+    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_5x5_cost_SCNAD800K05TD200.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    -p EXPERIMENT CTR_SCNACOST_${case} \
+    -p EXPERIMENT COST_SCNAD800K05TD200_${case} \
     -p CTR_ALG ${case} \
-    -p SELFISH 0.7 \
-    -p FILE "manhattan_grid_5X5_A.xml" \
+    -p SELFISH 0.5 \
+    -p KP 0.5 \
+    -p CO_KP 0.5 \
+    -p TD 200 \
+    -p CO_TD 200 \
+    -p DISTANCE_CONTROL 800 \
+    -p FILE "manhattan_grid_5X5_scenario_A.xml" \
     -p DEMAND_FILE "demand_scenario_A.csv" &"
     IT=$((IT+1))    
-    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_5x5_cost_dA.ipynb \
+    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_5x5_cost_SCNAD800K05TD200.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    -p EXPERIMENT CTR_SCNACOST_${case} \
+    -p EXPERIMENT COST_SCNAD800K05TD200_${case} \
     -p CTR_ALG ${case} \
-    -p SELFISH 0.7 \
-    -p FILE "manhattan_grid_5X5_A.xml" \
-    -p DEMAND_FILE "demand_scenario_A.csv" &     
+    -p SELFISH 0.5 \
+    -p KP 0.5 \
+    -p CO_KP 0.5 \
+    -p TD 200 \
+    -p CO_TD 200 \
+    -p DISTANCE_CONTROL 800 \
+    -p FILE "manhattan_grid_5X5_scenario_A.xml" \
+    -p DEMAND_FILE "demand_scenario_A.csv" &    
+done  
+
+# Scenario B
+
+for case in COST2 COST3 COST5 COST6
+do
+    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_5x5_cost_SCNBD800K05TD200.ipynb \
+    -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    -p EXPERIMENT COST_SCNBD800K05TD200_${case} \
+    -p CTR_ALG ${case} \
+    -p SELFISH 0.5 \
+    -p KP 0.5 \
+    -p CO_KP 0.5 \
+    -p TD 200 \
+    -p CO_TD 200 \
+    -p DISTANCE_CONTROL 800 \
+    -p FILE "manhattan_grid_5X5_scenario_B.xml" \
+    -p DEMAND_FILE "demand_scenario_B.csv" &"
+    IT=$((IT+1))    
+    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_5x5_cost_SCNBD800K05TD200.ipynb \
+    -p PATH_SYMUVIA ${PATH_SYMUVIA} \
+    -p EXPERIMENT COST_SCNBD800K05TD200_${case} \
+    -p CTR_ALG ${case} \
+    -p SELFISH 0.5 \
+    -p KP 0.5 \
+    -p CO_KP 0.5 \
+    -p TD 200 \
+    -p CO_TD 200 \
+    -p DISTANCE_CONTROL 800 \
+    -p FILE "manhattan_grid_5X5_scenario_B.xml" \
+    -p DEMAND_FILE "demand_scenario_B.csv" &     
 done  
 
 echo "Total Simulations: ${IT}"
