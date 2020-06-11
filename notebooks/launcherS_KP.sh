9053607f8282c98a9db18d1a531c527c4f857a15
Andres Ladino <aladinoster@gmail.com>
2020-06-11 12:46:50 +0200

    🚑  Fix for

--- notebooks/launcherS_KP.sh
+++ notebooks/launcherS_KP.sh
@@ -59,7 +59,7 @@ done
 # Scenario C
 
 # for k in 0.3 0.4 0.45 0.55 0.6 0.65 0.7 0.8
-for k in  0.01 0.05 0.1 0.15 0.17 0.19 0.2 0.21 0.23 0.25 0.3 0.5 0.6
+for k in 0.01 0.05 0.1 0.15 0.17 0.19 0.2 0.21 0.23 0.25 0.3 0.5 0.6
 do
     echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_KP_${k}_SCNCD${distance}.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
