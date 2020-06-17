1893266308f5e063095d5bd9a836baed2a662990
Andres Ladino <aladinoster@gmail.com>
2020-06-17 21:27:53 +0200

    🧪 Fix last test F

--- notebooks/launcherS_DISTANCE.sh
+++ notebooks/launcherS_DISTANCE.sh
@@ -90,11 +90,11 @@ do
     -p FILE "manhattan_grid_5X5_scenario_F.xml" \
     -p DEMAND_FILE "demand_scenario_F.csv" &"
     IT=$((IT+1))
-    papermill 01_Zone_Control.ipynb 01_Zone_Control_KP_${k}_SCNFD${d}.ipynb \
+    papermill 01_Zone_Control.ipynb 01_Zone_Control_KP_${kp}_SCNFD${d}.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    -p EXPERIMENT SKP_SCNFD${d}_${k} \
+    -p EXPERIMENT SKP_SCNFD${d}_${kp} \
     -p CTR_ALG ${case} \
-    -p KP ${k} \
+    -p KP ${kp} \
     -p DISTANCE_CONTROL ${d} \
     -p FILE "manhattan_grid_5X5_scenario_F.xml" \
     -p DEMAND_FILE "demand_scenario_F.csv" &
