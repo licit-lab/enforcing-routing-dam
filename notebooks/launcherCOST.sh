71452fb54a7292643801a03a38d8e2b727d25a14
Andres Ladino <aladinoster@gmail.com>
2020-06-18 15:52:43 +0200

    🧪 Test COSTN1-6: A+B

--- notebooks/launcherCOST.sh
+++ notebooks/launcherCOST.sh
@@ -6,63 +6,69 @@ IT=0
 # Cooperative strategies 
 
 # Scenario A
+kp=0.65
+td=0.0
+distance=800
 
-for case in COSTN1 COSTN6
+for case in COSTN1 COSTN2 COSTN3 COSTN4 COSTN5 COSTN6
 do
-    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_5x5_cost_SCNAD800K05TD200.ipynb \
+    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_5x5_cost_SCNAD${distance}K${kp}.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    -p EXPERIMENT COST_SCNAD800K05TD200_${case} \
+    -p EXPERIMENT manhattan_A_V2_3_800_5X5_${case}_${kp}_${td}_0.5_${kp}_${td}_X \
     -p CTR_ALG ${case} \
     -p SELFISH 0.5 \
-    -p KP 0.5 \
-    -p CO_KP 0.5 \
-    -p TD 200 \
-    -p CO_TD 200 \
-    -p DISTANCE_CONTROL 800 \
+    -p KP ${kp} \
+    -p CO_KP ${kp} \
+    -p TD ${td} \
+    -p CO_TD ${td} \
+    -p DISTANCE_CONTROL ${distance} \
     -p FILE "manhattan_grid_5X5_scenario_A.xml" \
     -p DEMAND_FILE "demand_scenario_A.csv" &"
     IT=$((IT+1))    
-    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_5x5_cost_SCNAD800K05TD200.ipynb \
+    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_5x5_cost_SCNAD${distance}K${kp}.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    -p EXPERIMENT COST_SCNAD800K05TD200_${case} \
+    -p EXPERIMENT manhattan_A_V2_3_800_5X5_${case}_${kp}_${td}_0.5_${kp}_${td}_X \
     -p CTR_ALG ${case} \
     -p SELFISH 0.5 \
-    -p KP 0.5 \
-    -p CO_KP 0.5 \
-    -p TD 200 \
-    -p CO_TD 200 \
-    -p DISTANCE_CONTROL 800 \
+    -p KP ${kp} \
+    -p CO_KP ${kp} \
+    -p TD ${td} \
+    -p CO_TD ${td} \
+    -p DISTANCE_CONTROL ${distance} \
     -p FILE "manhattan_grid_5X5_scenario_A.xml" \
-    -p DEMAND_FILE "demand_scenario_A.csv" &    
+    -p DEMAND_FILE "demand_scenario_A.csv" &  
 done  
 
 # Scenario B
+kp=0.65
+td=0.0
+distance=600
 
-for case in COSTN1 COSTN6
+for case in COSTN1 COSTN2 COSTN3 COSTN4 COSTN5 COSTN6
 do
-    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_5x5_cost_SCNBD800K05TD200.ipynb \
+    echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_5x5_cost_SCNBD${distance}K${kp}.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    -p EXPERIMENT COST_SCNBD800K05TD200_${case} \
+    -p EXPERIMENT manhattan_B_V2_3_800_5X5_${case}_${kp}_${td}_0.5_${kp}_${td}_X \
     -p CTR_ALG ${case} \
     -p SELFISH 0.5 \
-    -p KP 0.5 \
-    -p CO_KP 0.5 \
-    -p TD 200 \
-    -p CO_TD 200 \
-    -p DISTANCE_CONTROL 800 \
+    -p KP ${kp} \
+    -p CO_KP ${kp} \
+    -p TD ${td} \
+    -p CO_TD ${td} \
+    -p DISTANCE_CONTROL ${distance} \
     -p FILE "manhattan_grid_5X5_scenario_B.xml" \
     -p DEMAND_FILE "demand_scenario_B.csv" &"
     IT=$((IT+1))    
-    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_5x5_cost_SCNBD800K05TD200.ipynb \
+    papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_5x5_cost_SCNBD${distance}K${kp}.ipynb \
     -p PATH_SYMUVIA ${PATH_SYMUVIA} \
-    -p EXPERIMENT COST_SCNBD800K05TD200_${case} \
+    -p EXPERIMENT manhattan_B_V2_3_800_5X5_${case}_${kp}_${td}_0.5_${kp}_${td}_X \
     -p CTR_ALG ${case} \
     -p SELFISH 0.5 \
-    -p KP 0.5 \
-    -p CO_KP 0.5 \
-    -p TD 200 \
-    -p CO_TD 200 \
-    -p DISTANCE_CONTROL 800 \
+    -p KP ${kp} \
+    -p CO_KP ${kp} \
+    -p TD ${td} \
+    -p CO_TD ${td} \
+    -p DISTANCE_CONTROL ${distance} \
     -p FILE "manhattan_grid_5X5_scenario_B.xml" \
     -p DEMAND_FILE "demand_scenario_B.csv" &     
 done  
