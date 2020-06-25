1b12271e9076e74d907874b4e7444abeb95d738e
Andres Ladino <aladinoster@gmail.com>
2020-06-25 19:08:12 +0200

    🧪 Test Hybrid: Complete

--- notebooks/launcherHYBRID.sh
+++ notebooks/launcherHYBRID.sh
@@ -9,7 +9,6 @@ IT=0
 kp=0.65
 td=0.0
 distance=800
-cokp=0.2
 
 case="COSTH4"
 alpha=0.2
@@ -25,7 +24,8 @@ echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCN
 -p CO_TD ${td} \
 -p DISTANCE_CONTROL ${distance} \
 -p FILE "manhattan_grid_5X5_scenario_A.xml" \
--p DEMAND_FILE "demand_scenario_A.csv" &"
+-p DEMAND_FILE "demand_scenario_A.csv" 
+-p REF_SPEED "ref_speeds_25zones.csv" &"
 IT=$((IT+1))
 papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNAD${distance}K${kp}.ipynb \
 -p PATH_SYMUVIA ${PATH_SYMUVIA} \
@@ -38,7 +38,39 @@ papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNAD${di
 -p CO_TD ${td} \
 -p DISTANCE_CONTROL ${distance} \
 -p FILE "manhattan_grid_5X5_scenario_A.xml" \
--p DEMAND_FILE "demand_scenario_A.csv" &
+-p DEMAND_FILE "demand_scenario_A.csv"
+-p REF_SPEED "ref_speeds_25zones.csv"  &
+
+case="COSTN4"
+alpha=0.2
+
+echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNAD${distance}K${kp}.ipynb \
+-p PATH_SYMUVIA ${PATH_SYMUVIA} \
+-p EXPERIMENT manhattan_A_V2_3_${distance}_5X5_${case}_${kp}_${td}_${alpha}_X_X_X \
+-p CTR_ALG ${case} \
+-p SELFISH ${alpha} \
+-p KP ${kp} \
+-p CO_KP ${kp} \
+-p TD ${td} \
+-p CO_TD ${td} \
+-p DISTANCE_CONTROL ${distance} \
+-p FILE "manhattan_grid_5X5_scenario_A.xml" \
+-p DEMAND_FILE "demand_scenario_A.csv" 
+-p REF_SPEED "ref_speeds_25zones.csv" &"
+IT=$((IT+1))
+papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNAD${distance}K${kp}.ipynb \
+-p PATH_SYMUVIA ${PATH_SYMUVIA} \
+-p EXPERIMENT manhattan_A_V2_3_${distance}_5X5_${case}_${kp}_${td}_${alpha}_X_X_X \
+-p CTR_ALG ${case} \
+-p SELFISH ${alpha} \
+-p KP ${kp} \
+-p CO_KP ${kp} \
+-p TD ${td} \
+-p CO_TD ${td} \
+-p DISTANCE_CONTROL ${distance} \
+-p FILE "manhattan_grid_5X5_scenario_A.xml" \
+-p DEMAND_FILE "demand_scenario_A.csv"
+-p REF_SPEED "ref_speeds_25zones.csv"  &
 
 case="COSTH6"
 alpha=0.2
@@ -54,7 +86,8 @@ echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCN
 -p CO_TD ${td} \
 -p DISTANCE_CONTROL ${distance} \
 -p FILE "manhattan_grid_5X5_scenario_A.xml" \
--p DEMAND_FILE "demand_scenario_A.csv" &"
+-p DEMAND_FILE "demand_scenario_A.csv"
+-p REF_SPEED "ref_speeds_25zones.csv" &"
 IT=$((IT+1))
 papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNAD${distance}K${kp}.ipynb \
 -p PATH_SYMUVIA ${PATH_SYMUVIA} \
@@ -67,7 +100,39 @@ papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNAD${di
 -p CO_TD ${td} \
 -p DISTANCE_CONTROL ${distance} \
 -p FILE "manhattan_grid_5X5_scenario_A.xml" \
--p DEMAND_FILE "demand_scenario_A.csv" &
+-p DEMAND_FILE "demand_scenario_A.csv" 
+-p REF_SPEED "ref_speeds_25zones.csv" &
+
+case="COSTN6"
+alpha=0.2
+
+echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNAD${distance}K${kp}.ipynb \
+-p PATH_SYMUVIA ${PATH_SYMUVIA} \
+-p EXPERIMENT manhattan_A_V2_3_${distance}_5X5_${case}_${kp}_${td}_${alpha}_X_X_X \
+-p CTR_ALG ${case} \
+-p SELFISH ${alpha} \
+-p KP ${kp} \
+-p CO_KP ${kp} \
+-p TD ${td} \
+-p CO_TD ${td} \
+-p DISTANCE_CONTROL ${distance} \
+-p FILE "manhattan_grid_5X5_scenario_A.xml" \
+-p DEMAND_FILE "demand_scenario_A.csv" 
+-p REF_SPEED "ref_speeds_25zones.csv" &"
+IT=$((IT+1))
+papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNAD${distance}K${kp}.ipynb \
+-p PATH_SYMUVIA ${PATH_SYMUVIA} \
+-p EXPERIMENT manhattan_A_V2_3_${distance}_5X5_${case}_${kp}_${td}_${alpha}_X_X_X \
+-p CTR_ALG ${case} \
+-p SELFISH ${alpha} \
+-p KP ${kp} \
+-p CO_KP ${kp} \
+-p TD ${td} \
+-p CO_TD ${td} \
+-p DISTANCE_CONTROL ${distance} \
+-p FILE "manhattan_grid_5X5_scenario_A.xml" \
+-p DEMAND_FILE "demand_scenario_A.csv"
+-p REF_SPEED "ref_speeds_25zones.csv"  &
 
 # Scenario B
 
@@ -89,7 +154,38 @@ echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCN
 -p CO_TD ${td} \
 -p DISTANCE_CONTROL ${distance} \
 -p FILE "manhattan_grid_5X5_scenario_B.xml" \
--p DEMAND_FILE "demand_scenario_B.csv" &"
+-p DEMAND_FILE "demand_scenario_B.csv" 
+-p REF_SPEED "ref_speeds_25zones.csv" &"
+IT=$((IT+1))
+papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNBD${distance}K${kp}.ipynb \
+-p PATH_SYMUVIA ${PATH_SYMUVIA} \
+-p EXPERIMENT manhattan_B_V2_3_${distance}_5X5_${case}_${kp}_${td}_${alpha}_X_X_X \
+-p CTR_ALG ${case} \
+-p SELFISH ${alpha} \
+-p KP ${kp} \
+-p CO_KP ${kp} \
+-p TD ${td} \
+-p CO_TD ${td} \
+-p DISTANCE_CONTROL ${distance} \
+-p FILE "manhattan_grid_5X5_scenario_B.xml" \
+-p DEMAND_FILE "demand_scenario_B.csv" &
+
+case="COSTN4"
+alpha=0.2
+
+echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNBD${distance}K${kp}.ipynb \
+-p PATH_SYMUVIA ${PATH_SYMUVIA} \
+-p EXPERIMENT manhattan_B_V2_3_${distance}_5X5_${case}_${kp}_${td}_${alpha}_X_X_X \
+-p CTR_ALG ${case} \
+-p SELFISH ${alpha} \
+-p KP ${kp} \
+-p CO_KP ${kp} \
+-p TD ${td} \
+-p CO_TD ${td} \
+-p DISTANCE_CONTROL ${distance} \
+-p FILE "manhattan_grid_5X5_scenario_B.xml" \
+-p DEMAND_FILE "demand_scenario_B.csv" 
+-p REF_SPEED "ref_speeds_25zones.csv" &"
 IT=$((IT+1))
 papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNBD${distance}K${kp}.ipynb \
 -p PATH_SYMUVIA ${PATH_SYMUVIA} \
@@ -118,7 +214,8 @@ echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCN
 -p CO_TD ${td} \
 -p DISTANCE_CONTROL ${distance} \
 -p FILE "manhattan_grid_5X5_scenario_B.xml" \
--p DEMAND_FILE "demand_scenario_B.csv" &"
+-p DEMAND_FILE "demand_scenario_B.csv" 
+-p REF_SPEED "ref_speeds_25zones.csv" &"
 IT=$((IT+1))
 papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNBD${distance}K${kp}.ipynb \
 -p PATH_SYMUVIA ${PATH_SYMUVIA} \
@@ -131,4 +228,36 @@ papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNBD${di
 -p CO_TD ${td} \
 -p DISTANCE_CONTROL ${distance} \
 -p FILE "manhattan_grid_5X5_scenario_B.xml" \
--p DEMAND_FILE "demand_scenario_B.csv" &
+-p DEMAND_FILE "demand_scenario_B.csv" 
+-p REF_SPEED "ref_speeds_25zones.csv" &
+
+case="COSTN6"
+alpha=0.2
+
+echo "papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNBD${distance}K${kp}.ipynb \
+-p PATH_SYMUVIA ${PATH_SYMUVIA} \
+-p EXPERIMENT manhattan_B_V2_3_${distance}_5X5_${case}_${kp}_${td}_${alpha}_X_X_X \
+-p CTR_ALG ${case} \
+-p SELFISH ${alpha} \
+-p KP ${kp} \
+-p CO_KP ${kp} \
+-p TD ${td} \
+-p CO_TD ${td} \
+-p DISTANCE_CONTROL ${distance} \
+-p FILE "manhattan_grid_5X5_scenario_B.xml" \
+-p DEMAND_FILE "demand_scenario_B.csv" 
+-p REF_SPEED "ref_speeds_25zones.csv" &"
+IT=$((IT+1))
+papermill 01_Zone_Control.ipynb 01_Zone_Control_${case}_ALPHA_${alpha}_SCNBD${distance}K${kp}.ipynb \
+-p PATH_SYMUVIA ${PATH_SYMUVIA} \
+-p EXPERIMENT manhattan_B_V2_3_${distance}_5X5_${case}_${kp}_${td}_${alpha}_X_X_X \
+-p CTR_ALG ${case} \
+-p SELFISH ${alpha} \
+-p KP ${kp} \
+-p CO_KP ${kp} \
+-p TD ${td} \
+-p CO_TD ${td} \
+-p DISTANCE_CONTROL ${distance} \
+-p FILE "manhattan_grid_5X5_scenario_B.xml" \
+-p DEMAND_FILE "demand_scenario_B.csv" 
+-p REF_SPEED "ref_speeds_25zones.csv" &
