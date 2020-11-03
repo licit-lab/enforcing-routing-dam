3f8a3d736f4647344a497d29f577c9b7ede13c3a
Andres Ladino <aladinoster@gmail.com>
2020-11-03 06:47:07 +0100

    🧹 Clean and update of indicators

--- src/data/findpaths.py
+++ src/data/findpaths.py
@@ -45,9 +45,24 @@ import glob
 import os
 import pandas as pd
 from operator import itemgetter
-from .indicators import TTT_var, Mean_speed, MS_var, prc_rerouted, Dup, mean_routing, TWinLoose
+from indicators import (
+    TTT_var,
+    Mean_speed,
+    MS_var,
+    prc_rerouted,
+    Dup,
+    mean_routing,
+    TWinLoose,
+    networkHomogeneity,
+    TwinLooseDup,
+)
 
-from .dataloader import retreiveMFDData, retrieveVehData, retriveControlData, retriveReroutingData
+from dataloader import (
+    retreiveMFDData,
+    retrieveVehData,
+    retriveControlData,
+    retriveReroutingData,
+)
 
 
 def locate_filenames(general_path):
@@ -59,7 +74,9 @@ def locate_filenames(general_path):
         fullpaths = {}
         if not folder:
             xmlfile = glob.glob(path + "/*_traf.xml")
-            controlfile = glob.glob(path + "/defaultOut_ctrlzonedata_probabilities.csv")
+            controlfile = glob.glob(
+                path + "/defaultOut_ctrlzonedata_probabilities.csv"
+            )
             routingfile = glob.glob(path + "/defaultOut_ctrlzonedata_.csv")
             try:
                 fullpaths["xml"] = xmlfile[0]
@@ -97,9 +114,19 @@ def match_cases(dfCases):
     dfNoControl = dfCases[dfCases.ref]
     dfControl = dfCases[~dfCases.ref]
 
-    dfNoControl["key"] = dfNoControl.apply(lambda x: itemgetter(*[0, 1, 3, 4])(x["case"].split("_")), axis=1)
+    dfNoControl["key"] = dfNoControl.apply(
+        lambda x: itemgetter(*[0, 1, 4, 5])(x["case"].split("_")), axis=1
+    )
 
-    dfControl["key"] = dfControl.apply(lambda x: itemgetter(*[0, 1, 3, 5])(x["case"].split("_")), axis=1)
+    dfControl["key"] = dfControl.apply(
+        lambda x: itemgetter(*[0, 1, 3, 5])(x["case"].split("_")), axis=1
+    )
+
+    # For case processing tracking purposes
+    dfControl["keyfile"] = dfControl.apply(
+        lambda x: itemgetter(*[0, 1, 3, 4, 5, 6, 7])(x["case"].split("_")),
+        axis=1,
+    )
 
     dfNoControl.rename(columns={"xml": "noctr"}, inplace=True)
 
@@ -108,99 +135,255 @@ def match_cases(dfCases):
     return dfMatched
 
 
-if __name__ == "__main__":
-    gen_path = os.getcwd()
-    print(f"Analyzing: {gen_path}")
-    cases = locate_filenames(gen_path)
-    dfCases = pd.DataFrame(cases)
-    dfCases = dfCases.dropna(subset=["xml"])  # Cleaning non xml file folders
+# Moved to main
 
-    # This updates the dataframe with multiple columns 'key','noctr' denoting the corresponding non control scenario for each scenario
-    dfCases = match_cases(dfCases)
+# if __name__ == "__main__":
+#     gen_path = os.getcwd()
+#     print(f"Analyzing: {gen_path}")
+#     cases = locate_filenames(gen_path)
+#     dfCases = pd.DataFrame(cases)
+#     dfCases = dfCases.dropna(subset=["xml"])  # Cleaning non xml file folders
 
-    datalist = []
-    # Load all data
-    for index, row in dfCases.iterrows():
+#     # This updates the dataframe with multiple columns 'key','noctr' denoting the corresponding non control scenario for each scenario
+#     dfCases = match_cases(dfCases)
 
-        # Filenames for reference
+#     datalist = []
+#     # Load all data
+#     for index, row in dfCases.iterrows():
 
-        xmlfile = row.xml
-        ctrfile = row.ctr
-        roufile = row.rou
-        ncxmlfile = row.noctr
+#         # Filenames for reference
 
-        print(f"{categorize_path(xmlfile)} \t- {categorize_path(ncxmlfile)}")
+#         xmlfile = row.xml
+#         ctrfile = row.ctr
+#         roufile = row.rou
+#         ncxmlfile = row.noctr
 
-        # Retrieve closed loop data
-        dfMFD = retreiveMFDData(xmlfile)
-        dfVeh = retrieveVehData(xmlfile)
-        dfRoute = retriveReroutingData(roufile)
-        dfCtr = retriveControlData(ctrfile)
+#         print(f"{categorize_path(xmlfile)} \t- {categorize_path(ncxmlfile)}")
 
-        # Retreive Open loop data
-        dfMFDNC = retreiveMFDData(ncxmlfile)
-        dfVehNC = retrieveVehData(ncxmlfile)
+#         # Retrieve closed loop data
+#         dfMFD = retreiveMFDData(xmlfile)
+#         dfVeh = retrieveVehData(xmlfile)
+#         dfRoute = retriveReroutingData(roufile)
+#         dfCtr = retriveControlData(ctrfile)
 
-        # Indicators computation
-        TTTVar = TTT_var(dfMFD, dfMFDNC)
-        MeanSpeed = Mean_speed(dfMFD)
-        MeanSpeedNC = Mean_speed(dfMFDNC)
-        MSVar = MS_var(dfMFD, dfMFDNC)
-        PrcRouted = prc_rerouted(dfVeh, dfVehNC)
-        DUp, DUpp, DUp5, DUp10 = Dup(dfVeh, dfVehNC)
-        MeanTwin, MeanTLoose, TLoose10, TLoose5, PrcTwin, PrcTLoose = TWinLoose(dfVeh, dfVehNC)
-        MeanRouting, StdRouting, VarRouting = mean_routing(dfRoute)
+#         # Retreive Open loop data
+#         dfMFDNC = retreiveMFDData(ncxmlfile)
+#         dfVehNC = retrieveVehData(ncxmlfile)
 
-        network, scenario, routev, T, dmin, grid, ctrl, kp, td, alpha, cokp, cotd, beta = categorize_path(
-            xmlfile
-        ).split("_")
+#         # Indicators computation
+#         TTTVar = TTT_var(dfMFD, dfMFDNC)
+#         MeanSpeed = Mean_speed(dfMFD)
+#         MeanSpeedNC = Mean_speed(dfMFDNC)
+#         MSVar = MS_var(dfMFD, dfMFDNC)
+#         PrcRouted = prc_rerouted(dfVeh, dfVehNC)
+#         (
+#             DUp,
+#             DUpp,
+#             DUp5,
+#             DUp10,
+#             PrcTwin,
+#             PrcTLoose,
+#             MeanTwin,
+#             MeanTLoose,
+#             TLoose10,
+#             TLoose5,
+#         ) = TwinLooseDup(dfVeh, dfVehNC)
+#         # DUp, DUpp, DUp5, DUp10 = Dup(dfVeh, dfVehNC)
+#         # MeanTwin, MeanTLoose, TLoose10, TLoose5, PrcTwin, PrcTLoose = TWinLoose(
+#         #     dfVeh, dfVehNC
+#         # )
+#         MeanRouting, StdRouting, VarRouting = mean_routing(dfRoute)
+#         NetHomogeneity = networkHomogeneity(dfMFD)
 
-        data = {
-            "network": network,
-            "scenario": scenario,
-            "routeversion": routev,
-            "T": T,
-            "dmin": dmin,
-            "grid": grid,
-            "ctrl": ctrl,
-            "kp": kp,
-            "X": td,
-            "Y": cokp,
-            "Z": beta,
-            "TTT_var": TTTVar["sensor_network"],
-            "TTT_var_i": TTTVar.drop(["sensor_network", "sensor_outer_ring"]),
-            "Mean_speed": MeanSpeed["sensor_network"],
-            "Mean_speed_i": MeanSpeed.drop(["sensor_network", "sensor_outer_ring"]),
-            "MS_var": MSVar["sensor_network"],
-            "MS_var_i": MSVar.drop(["sensor_network", "sensor_outer_ring"]),
-            "pct_routedveh": PrcRouted,
-            "Dup": DUp,
-            "Dup+": DUpp,
-            "Dup_10": DUp10,
-            "Dup_5": DUp5,
-            "%Twin": PrcTwin,
-            "%Tloose": PrcTLoose,
-            "Twin": MeanTwin,
-            "Tloose": MeanTLoose,
-            "Tloose_10": TLoose10,
-            "Tloose_5": TLoose5,
-            "Mean_routing": MeanRouting,
-            "Var_routing": VarRouting,
-        }
+#         (
+#             network,
+#             scenario,
+#             routev,
+#             T,
+#             dmin,
+#             grid,
+#             ctrl,
+#             kp,
+#             td,
+#             alpha,
+#             cokp,
+#             cotd,
+#             beta,
+#         ) = categorize_path(xmlfile).split("_")
 
-        datalist.append(pd.DataFrame(data))
+#         data = {
+#             "network": network,
+#             "scenario": scenario,
+#             "routeversion": routev,
+#             "T": T,
+#             "dmin": dmin,
+#             "grid": grid,
+#             "ctrl": ctrl,
+#             "kp": kp,
+#             "td": td,
+#             "cokp": cokp,
+#             "beta": beta,
+#             "alpha": alpha,
+#             "TTT_var": TTTVar["sensor_network"],
+#             "TTT_var_i": TTTVar.drop(["sensor_network",]),
+#             "Mean_speed": MeanSpeed["sensor_network"],
+#             "Mean_speed_i": MeanSpeed.drop(["sensor_network",]),
+#             "MS_var": MSVar["sensor_network"],
+#             "MS_var_i": MSVar.drop(["sensor_network"]),
+#             "pct_routedveh": PrcRouted,
+#             "Dup": DUp,
+#             "Dup+": DUpp,
+#             "Dup_10": DUp10,
+#             "Dup_5": DUp5,
+#             "%Twin": PrcTwin,
+#             "%Tloose": PrcTLoose,
+#             "Twin": MeanTwin,
+#             "Tloose": MeanTLoose,
+#             "Tloose_10": TLoose10,
+#             "Tloose_5": TLoose5,
+#             "Mean_routing": MeanRouting,
+#             "Var_routing": VarRouting,
+#             "Net_homogeneity": NetHomogeneity,
+#         }
 
-    df = pd.concat(datalist)
+#         datalist.append(pd.DataFrame(data))
 
-    df = df.reset_index()
+#     df = pd.concat(datalist)
 
-#     import matplotlib.pyplot as plt
+#     df = df.reset_index()
 
-# field = 'pct_routedveh'
-# df2plot = df[['scenario','ctrl',field]].groupby(['scenario','ctrl']).mean()
-# ax = df2plot.plot(kind='bar',title='% Rerouted Vehicles')
-# ax.get_legend().remove()
-# i = 0
-# for d,r in df2plot.iterrows():
-#     plt.annotate(round(r[field],2),xy=(i-0.4, r[field]/2), rotation=30, color = 'purple')
-#     i+=1
+# ---
+
+# import matplotlib.pyplot as plt
+# from matplotlib import cm
+# from itertools import product
+
+# fields = (
+#     "TTT_var",
+#     "%Twin",
+#     "%Tloose",
+#     "Dup",
+#     "Dup+",
+#     "Net_homogeneity",
+#     "pct_routedveh",
+# )
+# title = {
+#     "TTT_var": "[%] - TTTVar",
+#     "%Twin": "[%] - Twin",
+#     "%Tloose": "[%] - Tloose",
+#     "Dup": "[%] - Dup",
+#     "Dup+": "[%] - Dup+",
+#     "Net_homogeneity": "[m/s] - Network homogeneity",
+#     "pct_routedveh": "[%] - Rerouted Vehicles",
+# }
+# scenarios = ("A", "B")
+
+# for scenario, field in product(scenarios, fields):
+#     print(type(field), type(scenario))
+
+#     df2plot = (
+#         df[["scenario", "ctrl", "alpha", field]]
+#         .groupby(["scenario", "ctrl", "alpha",])
+#         .mean()
+#     )
+#     df2plot = df2plot.loc[(scenario, slice(None), slice(None)), :]
+
+#     # tst = df2plot.copy()
+#     # s2t = tst.unstack("grid")
+
+#     # fig, ax = plt.subplots(1, 4, figsize=(15, 7))
+#     # tab10 = cm.get_cmap("tab10")
+#     # colors = ["blue", "orange", "green", "purple"]
+#     # for x, a, c in zip(s2t, ax.flatten(), colors):
+#     #     s2t[x].plot(kind="bar", ax=a, title=x[1], color="cornflowerblue")
+#     #     posx = [x for x in range(5)]
+#     #     posy = [v / 2 for v in s2t[x]]
+#     #     values = list(round(s2t[x], 2))
+#     #     xyp = [(x, y) for x, y in zip(posx, posy)]
+#     #     [
+#     #         a.annotate(v, xy=an, rotation=30, color="purple",)
+#     #         for v, an in zip(values, xyp)
+#     #     ]
+#     #     a.axhline(s2t.loc[(scenario, "P", "X"), x], color="red")
+
+#     # fig.suptitle(f"Scenario {scenario} - {title.get(field)}")
+#     ax = df2plot.plot(
+#         kind="bar",
+#         title=f"Scenario {scenario} - {title.get(field)}",
+#         color="cornflowerblue",
+#     )
+#     ax.get_legend().remove()
+#     i = 0
+#     for d, r in df2plot.iterrows():
+#         plt.annotate(
+#             round(r[field], 2),
+#             xy=(i - 0.4, r[field] / 2),
+#             rotation=30,
+#             color="purple",
+#         )
+#         i += 1
+
+#     ax.axhline(df2plot.loc[(scenario, "P", "X"), field], color="red")
+#     # plt.show()
+#     plt.tight_layout()
+#     plt.savefig(
+#         "/Users/ladino/Desktop/29062020/COST4old/"
+#         + scenario
+#         + "_"
+#         + field
+#         + ".png"
+#     )
+# # Find optimal case:
+
+# cases = (
+#     df[["scenario", "ctrl", "TTT_var", "Dup+"]]
+#     .groupby(["scenario", "ctrl"])
+#     .mean()
+# )
+# casesA = cases.loc[("A", slice(None), slice(None)), :]
+# # casesA = casesA.loc[(slice(None), "COSTN6", slice(None)), :]
+# # casesA["TTT_var"] = -casesA["TTT_var"]
+
+# casesB = cases.loc[("B", slice(None), slice(None)), :]
+# # casesB = casesB.loc[(slice(None), "COSTN6", slice(None)), :]
+# # casesB["TTT_var"] = -casesB["TTT_var"]
+
+# # Global optima
+# casesA.loc[("A", "Min"), :] = [-16.71, 2.73]
+# casesA.loc[("A", "Max"), :] = [0, 32.03]
+
+# casesB.loc[("B", "Min"), :] = [-30.65, 3]
+# casesB.loc[("B", "Max"), :] = [0, 36]
+
+# from sklearn import preprocessing
+
+# # min_max_scaler = preprocessing.MinMaxScaler()
+# casesApproc = casesA.copy()
+# # Find delta
+# deltaA = casesA.loc[("A", "Max")] - casesA.loc[("A", "Min")]
+# # Find min
+# minA = casesA.loc[("A", "Min")]
+# # Normalization
+# casesApproc = (casesApproc - minA).divide(deltaA)
+
+# # casesApproc.loc[:, :] = min_max_scaler.fit_transform(casesA.values)
+# casesApproc["J"] = (
+#     casesApproc.loc[(slice(None), ["COSTN4", "COSTN6"]), :] * 0.5
+# ).sum(axis=1)
+# opt_case_A = casesApproc["J"].idxmin()
+# print(opt_case_A)
+
+# casesBpproc = casesB.copy()
+
+# # Find delta
+# deltaB = casesB.loc[("B", "Max")] - casesB.loc[("B", "Min")]
+# # Find min
+# minB = casesB.loc[("B", "Min")]
+# # Normalization
+# casesBpproc = (casesBpproc - minB).divide(deltaB)
+# # casesBpproc.loc[:, :] = min_max_scaler.fit_transform(casesB.values)
+# casesBpproc["J"] = (
+#     casesBpproc.loc[(slice(None), ["COSTN4", "COSTN6"]), :] * 0.5
+# ).sum(axis=1)
+# opt_case_B = casesBpproc["J"].idxmin()
+# print(opt_case_B)
--- src/data/indicators.py
+++ src/data/indicators.py
@@ -10,14 +10,18 @@ def pivotTTT(XMLDataFrame):
     """ 
         Pivot original DF to have TTT data in columns per sensor along time 
     """
-    return XMLDataFrame.pivot(index="fin", columns="id", values="temps_total_passe")
+    return XMLDataFrame.pivot(
+        index="fin", columns="id", values="temps_total_passe"
+    )
 
 
 def pivotTTD(XMLDataFrame):
     """ 
         Pivot original DF to have TTT data in columns per sensor along time 
     """
-    return XMLDataFrame.pivot(index="fin", columns="id", values="distance_totale_parcourue")
+    return XMLDataFrame.pivot(
+        index="fin", columns="id", values="distance_totale_parcourue"
+    )
 
 
 def computeTTT(XMLDataFrame):
@@ -56,7 +60,10 @@ def Mean_speed(XMLDataFrame):
     """ 
         Compute TTT Variation w.r.t scenario of reference
     """
-    return compute_SPD(XMLDataFrame).mean()
+    TTD = pivotTTD(XMLDataFrame)
+    TTT = pivotTTT(XMLDataFrame)
+
+    return TTD.sum() / TTT.sum()
 
 
 def MS_var(XMLDataFrame, XMLDataFrameRef):
@@ -74,9 +81,21 @@ def prc_rerouted(XMLDataFrame, XMLDataFrameRef):
     """ 
         Compute the % of rerouted vehicles
     """
-    diffPaths = pd.concat([XMLDataFrame["itineraire"], XMLDataFrameRef["itineraire"]]).drop_duplicates(keep=False)
-    nChangedPaths = diffPaths.count()
-    nGeneratedVeh = XMLDataFrameRef["itineraire"].count()
+    # diffPaths = pd.concat(
+    #     [XMLDataFrame["itineraire"], XMLDataFrameRef["itineraire"]]
+    # ).drop_duplicates(keep=False)
+    # nChangedPaths = diffPaths.count()
+    # nGeneratedVeh = XMLDataFrameRef["itineraire"].count()
+    nChangedPaths = len(
+        [
+            i
+            for i, j in zip(
+                XMLDataFrame["itineraire"], XMLDataFrameRef["itineraire"]
+            )
+            if i != j
+        ]
+    )
+    nGeneratedVeh = len(XMLDataFrame["id"])
     return nChangedPaths / nGeneratedVeh * 100
 
 
@@ -93,7 +112,9 @@ def Dup(XMLDataFrame, XMLDataFrameRef):
     """
         Mean of increase of individual travel times 
     """
-    Dup = (XMLDataFrame["dstParcourue"] - XMLDataFrameRef["dstParcourue"]).divide(XMLDataFrameRef["dstParcourue"]) * 100
+    Dup = (
+        XMLDataFrame["dstParcourue"] - XMLDataFrameRef["dstParcourue"]
+    ).divide(XMLDataFrameRef["dstParcourue"]) * 100
     Dupp = Dup[Dup > 2]
     Dup5 = np.percentile(Dup, 95)
     Dup10 = np.percentile(Dup, 90)
@@ -104,6 +125,7 @@ def TWinLoose(XMLDataFrame, XMLDataFrameRef):
     """ 
         Mean % of Time Gained 
     """
+
     TTT = computeTTT(XMLDataFrame)
     TTTNC = computeTTT(XMLDataFrameRef)
     TSN = TTT - TTTNC
@@ -125,9 +147,110 @@ def TWinLoose(XMLDataFrame, XMLDataFrameRef):
     )
 
 
+def TwinLooseDup(XMLDataFrame, XMLDataFrameRef):
+
+    idvehs = list(XMLDataFrame.id)
+
+    ttds = []
+    ttts = []
+
+    for i, r in XMLDataFrame.iterrows():
+        if r["instS"] != "-1.00":
+            ttds.append(r["dstParcourue"])
+            ttts.append(r["instS"] - r["instC"])
+        else:
+            ttds.append(-1.0)
+            ttts.append(-1.0)
+
+    ttts_ref = []
+    ttds_ref = []
+
+    for i, r in XMLDataFrameRef.iterrows():
+        if r["instS"] != "-1.00":
+            ttds_ref.append(r["dstParcourue"])
+            ttts_ref.append(r["instS"] - r["instC"])
+        else:
+            ttds_ref.append(-1.0)
+            ttts_ref.append(-1.0)
+
+    tds_win = []
+    v_tds_win = []
+    tds_loose = []
+    v_tds_loose = []
+    dds = []
+
+    for v in range(len(idvehs)):
+
+        if ttds[v] > 0 and ttts[v] > 0 and ttds_ref[v] > 0 and ttts_ref[v] > 0:
+            dds.append((ttds[v] - ttds_ref[v]) / ttds_ref[v] * 100)
+
+    for v in range(len(idvehs)):
+        # if paths[v]!=paths_ref[v] and ttds[v]>0 and ttts[v]>0 and ttds_ref[v]>0 and ttts_ref[v]>0:
+        if ttds[v] > 0 and ttts[v] > 0 and ttds_ref[v] > 0 and ttts_ref[v] > 0:
+            dds.append((ttds[v] - ttds_ref[v]) / ttds_ref[v] * 100)
+
+            if ttts[v] < ttts_ref[v]:
+                v_tds_win.append(idvehs[v])
+                tds_win.append(-(ttts[v] - ttts_ref[v]) / ttts_ref[v] * 100)
+
+            if ttts[v] > ttts_ref[v]:
+                v_tds_loose.append(idvehs[v])
+                tds_loose.append((ttts[v] - ttts_ref[v]) / ttts_ref[v] * 100)
+
+    Dup = sum(dds) / float(len(dds))
+
+    # Dup+
+    dds_supp = []
+    for d in dds:
+        if d > 0.02:
+            dds_supp.append(d)
+
+    Dupp = sum(dds_supp) / float(len(dds_supp))
+    # print('Dup',Dupp)
+
+    # Dup_10
+    Dup_10 = round(np.percentile(dds, 90), 2)
+
+    # Dup_5
+    Dup_5 = round(np.percentile(dds, 95), 2)
+
+    # %Twin
+    PrcTwin = round(len(v_tds_win) / (len(v_tds_win) + len(v_tds_loose)), 2)
+
+    # %Tloose
+    PrcTloose = round(len(v_tds_loose) / (len(v_tds_win) + len(v_tds_loose)), 2)
+
+    # Twin
+    Twin = sum(tds_win) / float(len(tds_win))
+    # print('Twin',Twin)
+
+    # Tloose
+    Tloose = sum(tds_loose) / float(len(tds_loose))
+    # print('Tloose',Tloose)
+
+    # Tloose_10
+    Tloose_10 = round(np.percentile(tds_loose, 90), 2)
+
+    # Tloose_5
+    Tloose_5 = round(np.percentile(tds_loose, 95), 2)
+
+    return (
+        Dup,
+        Dupp,
+        Dup_10,
+        Dup_5,
+        PrcTwin,
+        PrcTloose,
+        Twin,
+        Tloose,
+        Tloose_10,
+        Tloose_5,
+    )
+
+
 def networkHomogeneity(XMLDataFrame):
     """
         Variance of average speeds in zones
     """
     SPD = compute_SPD(XMLDataFrame)
-    return SPD.mean().drop(["sensor_network", "sensor_outer_ring"]).std()
+    return SPD.mean().drop(["sensor_network"]).std()
